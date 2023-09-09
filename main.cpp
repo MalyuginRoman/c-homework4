e0f5a49373dd817a7aca9f375a92c026a302d37b
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2023-09-09 08:41:50 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,106 +1,111 @@
 #include <iostream>
-#include <cmath>
+#include <type_traits>
 #include <list>
 #include <vector>
 #include <tuple>
+#include <string>
 
-#include "lib.h"
+#inclide 'lib.h'
 
-namespace integer {
-	
-	template <typename T>
-	auto print_ip(T &number) -> decltype(number) {
-		unsigned char uc;
-		int SizeInt = sizeof(T);
-		switch(SizeInt)
-		{
-			case 1:
-				uc = (unsigned char) number;
-				std::cout << (int) uc;
-				break;
-			default:
-				for (int i = SizeInt; i--;) {
-					int8_t a = number / pow(256, i);
-					uc = (unsigned char) a;
-					std::cout << (int) uc;
-					if (i > 0) std::cout << ".";
-					number -= (int) uc * pow(256, i);
-				}
-				break;
-		}
-		std::cout << std::endl;
-		return number;
-	}
-	void print_ip(std::string a) {
-		std::cout << a << '\n';
-	}
+static const char tchk = '.';
 
-	void example() {
-		int8_t  int8 = -1;
-		print_ip(int8);
-		int16_t int16 = 0;
-		print_ip(int16);
-		int32_t int32 = 2130706433;
-		print_ip(int32);
-		int64_t int64 = 8875824491850138409;
-		print_ip(int64);
-		std::string string = "Hello, World!";
-		print_ip(string);
-	}
+template <typename T>
+static std::enable_if_t<std::is_integral_v<T>, void> PrintIp(T value)
+{
+    static const size_t constexpr SizeInOctets = sizeof(T);
+    for (size_t i = SizeInOctets; i--;)
+    {
+        std::cout << (value >> (i << 3) & 0xFF);
+        if (i > 0)
+            std::cout << tchk;
+    }
+    std::cout << std::endl;
+}
 
-} // integer
+static void PrintIp(const std::string& value)
+{
+    std::cout << value << std::endl;
+}
 
-namespace container {
+template <typename>
+struct is_std_vector_or_list : std::false_type {};
 
-	template <typename T>			// vector
-	auto print_container(T &container, long) -> decltype(container) {
-		int count = container.size();
-		for(int i = 0; i < count; i++) {
-			if(i < (count - 1)) std::cout << container.at(i) << '.';
-			else std::cout << container.at(i) << '\n';
-		}
-	}
+template <typename T, typename... Args>
+struct is_std_vector_or_list<std::vector<T, Args...>> : std::true_type {};
 
-	template <typename T>			// list
-	auto print_container(T &container, int) -> void {
-		int count = container.size();
-		int iter = 0;
-		for(int n : container) {
-			iter ++;
-			if(iter < count) std::cout << n << '.';
-			else std::cout << n << '\n';
-		}
-	}
+template <typename T, typename... Args>
+struct is_std_vector_or_list<std::list<T, Args...>> : std::true_type {};
 
-	template<typename T>
-	void print_container(T &container) {
-		container::print_container(container, 42);
-	}
+template <typename T>
+static void PrintIp(std::enable_if_t<is_std_vector_or_list<T>::value, const T&> container)
+{
+    for (auto it = container.begin(); it != container.end(); ++it)
+    {
+        std::cout << *it;
+        if (it != std::prev(container.end()))
+            std::cout << tchk;
+    }
+    std::cout << std::endl;
+}
 
-	void example() {
-		std::vector<int> vector{100, 200, 300, 400};
-		print_container(vector);
-		std::list<short> list{400, 300, 200, 100};
-		print_container(list);
-	}
-} // container
+template <size_t I>
+struct PrintTuple
+{
+    template <typename Type0, typename... Types>
+    static constexpr void Print(const std::tuple<Type0, Types...>& tuple)
+    {
+        auto element = std::get<sizeof...(Types) + 1 - I>(tuple);
+        static_assert (std::is_same_v<Type0, decltype(element)>, "different types in tuple!");
 
-namespace tuple {
+        std::cout << element;
+        if (I > 1)
+            std::cout << tchk;
 
-	void example() {
-		std::tuple<int, int, int, int> tup;
-		tup = std::make_tuple(123, 456, 789, 0);
-		std::cout << std::get<0>(tup) << '.' << std::get<1>(tup) << '.' 
-				  << std::get<2>(tup) << '.' << std::get<3>(tup) << std::endl;
-	}
-} // tuple
+        PrintTuple<I - 1>::Print(tuple);
+    }
+};
 
-int main() {
+template <>
+struct PrintTuple<0>
+{
+    template <typename... Types>
+    static constexpr void Print(const std::tuple<Types...>&) {}
+};
 
-    std::cout << "Version: " << version() << std::endl;
-	integer::example();
-	container::example();
-	tuple::example();
 
-	return 0;
+template <typename... Types>
+static void PrintIp(const std::tuple<Types...>& tuple)
+{
+    PrintTuple<sizeof...(Types)>::Print(tuple);
+    std::cout << std::endl;
+}
+
+int main(int, char const **)
+{
+    int8_t int8{-1};
+    PrintIp(int8);
+
+    int16_t int16{0};
+    PrintIp(int16);
+
+    int32_t int32{2130706433};
+    PrintIp(int32);
+
+    int64_t int64{8875824491850138409};
+    PrintIp(int64);
+
+    PrintIp("Hello, World!");
+
+    using IntVector = std::vector<int>;
+    IntVector v{100, 200, 300, 400};
+    PrintIp<IntVector>(v);
+
+    using ShortList = std::list<short>;
+    ShortList l{400, 300, 200, 100};
+    PrintIp<ShortList>(l);
+
+    std::tuple<int, int, int, int> t{123, 456, 789, 0};
+    PrintIp(t);
+
+    return 0;
 }
